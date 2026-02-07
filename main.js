3c68de6750f618aaac6df8a36a60b5baa99db5a9
ferliest <ferliest@outlook.com>
2026-02-07 14:35:19 +0200

    leptomereies2

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ const STAGES = {
   end: {
   bg: "assets/olympus.jpg",
   title: "The end",
-  text: `You found all the treasures.<br>`,
+  text: `<b>Great Job.</b><br><br>You found all the treasures.`,
   buttons: [
     { label: "Play Again", action: () => goTo("intro", true) }
   ]
