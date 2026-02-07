73389f512acc8cfce25650c50741a15c1bc9aee5
ferliest <ferliest@outlook.com>
2026-02-07 14:27:28 +0200

    leptomereia

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ const STAGES = {
   end: {
   bg: "assets/olympus.jpg",
   title: "The end",
-  text: `You found all the treasures.<br>;`,
+  text: `You found all the treasures.<br>`,
   buttons: [
     { label: "Play Again", action: () => goTo("intro", true) }
   ]
